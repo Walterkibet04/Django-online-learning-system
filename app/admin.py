9e7857d2bd2282d421efd9a580472ffbd9d389cf
Walter kibet <kibetwalter726@gmail.com>
2023-11-29 14:07:41 +0300

    first commit

---
+++ app/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from .models import *
+
+
+# Register your models here.
+
+class what_you_learn_TabularInline(admin.TabularInline):
+    model = What_you_learn
+
+class Requirements_TabularInline(admin.TabularInline):
+    model = Requirements
+
+class Video_TabularInline(admin.TabularInline):
+    model = Video
+
+class course_admin(admin.ModelAdmin):
+    inlines = (what_you_learn_TabularInline, Requirements_TabularInline, Video_TabularInline)
+
+admin.site.register(Categories)
+admin.site.register(Author)
+admin.site.register(Course, course_admin)
+admin.site.register(Level)
+admin.site.register(Requirements)
+admin.site.register(Lesson)
+admin.site.register(UserCourse)
